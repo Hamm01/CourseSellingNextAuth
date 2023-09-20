b2dc328a6a4676b274559dbf9f5664bb2ca27d62
Himanish kochhar <himanish.kochhar@gmail.com>
2023-09-20 15:34:57 +0530

    Middleware updated for cookies issue on production

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,8 +6,17 @@ export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname
 
   const ispublicPath = path === '/signin' || path === '/signup'
+  let token = ''
 
-  const token = request.cookies.get('next-auth.session-token')?.value || ''
+  if (process.env.NODE_ENV === 'production') {
+    token =
+      request.cookies.get(`${process.env.NEXTAUTH_SESSION_COOKIE_DEV}`)
+        ?.value || ''
+  } else {
+    token =
+      request.cookies.get(`${process.env.NEXTAUTH_SESSION_COOKIE_PROD}`)
+        ?.value || ''
+  }
 
   if (ispublicPath && token) {
     return NextResponse.redirect(new URL('/', request.nextUrl))
